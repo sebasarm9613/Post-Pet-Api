1ba75dc2e4fbbfb2abb93e0f83acd7bcae0d20a6
Sebasarm96 <sebasarm96@gmail.com>
2025-03-21 21:45:54 -0500

    actualizacion2

--- src/presentation/services/user.service.ts
+++ src/presentation/services/user.service.ts
@@ -4,76 +4,90 @@ import { CreateUserDTO } from '../../domain/dtos/users/create-user.dto';
 import { UpdateUserDTO } from '../../domain/dtos/users/update-user.dto';
 
 export class UserService {
-	async findOne(id: string) {
-		const user = await User.findOne({
-			select: ['id', 'name', 'email', 'role'],
-			where: {
-				status: true,
-				id: id,
-			},
-		});
+  async findOne(id: string) {
+    const user = await User.findOne({
+      select: ['id', 'name', 'email', 'role'],
+      where: {
+        status: true,
+        id: id,
+      },
+    });
 
-		if (!user) {
-			throw CustomError.notFound('User not found');
-		}
-		return user;
-	}
+    if (!user) {
+      throw CustomError.notFound('User not found');
+    }
+    return user;
+  }
 
-	async findAll() {
-		try {
-			const users = await User.find({
-				select: ['id', 'name', 'email', 'role'],
-				where: {
-					status: true,
-				},
-			});
-			return users;
-		} catch (error) {
-			throw CustomError.internalServer('Error fetching users');
-		}
-	}
+  async findAll() {
+    try {
+      const users = await User.find({
+        select: ['id', 'name', 'email', 'role'],
+        where: {
+          status: true,
+        },
+      });
+      return users;
+    } catch (error) {
+      throw CustomError.internalServer('Error fetching users');
+    }
+  }
 
-	async create(data: CreateUserDTO) {
-		const user = new User();
+  async create(data: CreateUserDTO) {
+    const user = new User();
 
-		user.name = data.name;
-		user.email = data.email;
-		user.password = data.password;
-		// user.role = data.role;
+    user.name = data.name;
+    user.email = data.email;
+    user.password = data.password;
+    // user.role = data.role;
 
-		try {
-			return await user.save();
-		} catch (error) {
-			throw CustomError.internalServer('Error creating user');
-		}
-	}
+    try {
+      return await user.save();
+    } catch (error) {
+      throw CustomError.internalServer('Error creating user');
+    }
+  }
 
-	async update(id: string, data: UpdateUserDTO) {
-		const user = await this.findOne(id);
+  async update(id: string, data: UpdateUserDTO) {
+    const user = await this.findOne(id);
 
-		user.name = data.name;
-		user.email = data.email;
+    user.name = data.name;
+    user.email = data.email;
 
-		try {
-			await user.save();
-			return {
-				message: 'User updated successfully',
-			};
-		} catch (error) {
-			throw CustomError.internalServer('Error updating user');
-		}
-	}
+    try {
+      await user.save();
+      return {
+        message: 'User updated successfully',
+      };
+    } catch (error) {
+      throw CustomError.internalServer('Error updating user');
+    }
+  }
 
-	async delete(id: string) {
-		const user = await this.findOne(id);
+  async delete(id: string) {
+    const user = await this.findOne(id);
 
-		user.status = false;
+    user.status = false;
 
-		try {
-			await user.save();
-			return { ok: true };
-		} catch (error) {
-			throw CustomError.internalServer('Error deleting user');
-		}
-	}
+    try {
+      await user.save();
+      return { ok: true };
+    } catch (error) {
+      throw CustomError.internalServer('Error deleting user');
+    }
+  }
 }
+
+//   async delete(id: string) {
+//     const user = await this.findOne(id);
+
+//     try {
+//       await user.remove();
+//       return {
+//         message: ' User deleted successfully',
+//       };
+//     } catch (error) {
+//       throw CustomError.internalServer(' Error deleting pet post');
+//     }
+//   }
+// }
